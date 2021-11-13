3e1077e6b03f3cb018629dd84c7a7fd6193d2dbf
Dave Stewart <davs2rt@gmail.com>
2021-11-12 19:12:39 -0800

    pulled some configuration from the toy

---
+++ streamer/conf.py
@@ -0,0 +1,13 @@
+# ACCOUNT = 'DU2326883'  # arden9050 #2
+# ACCOUNT = 'DU2856241'  # btcvc4842
+ACCOUNT = "DU1825086"  # btcadv966
+ACCOUNT = "DF3987931"  # btcjo7537
+MIN_DELTA = 0.1
+MAX_DELTA = 0.7
+RANGES_NUM_DAYS = 14
+INDICATOR_WINDOW_SIZE = 100
+ADX_PERIOD = 27
+PERCENTILE_WIDTH = 5
+# 1=Live, 2=Frozen, 3=Delayed, 4=Delayed Frozen   for explanation see:
+# https://interactivebrokers.github.io/tws-api/market_data_type.html
+MARKET_DATA_TYPE = 4
