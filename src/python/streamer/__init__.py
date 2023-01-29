84d331a031fc8fbc391b5f4413735a6fc4123c64
Bill Rees <brees@dustyrobotics.com>
2023-01-28 20:53:32 -0800

    Fill in __init__.py to announce classes and vars

---
+++ src/python/streamer/__init__.py
@@ -0,0 +1,30 @@
+
+
+from .candles import (
+        Candle, CandleMakerBase, CandleMakerTimed, CandleMakerCounted, CandleMakerDollarVolume
+        )
+from .connect import (
+    Gateway, TradersWorkstation, Btcjopaper, Btchfpaper, Nypaperib,
+    Chpaperib, GatewayFromEnvironment, Connection,
+    )
+from .collect import (
+Gateway,
+TradersWorkstation,
+Btcjopaper,
+Btchfpaper,
+Nypaperib,
+Chpaperib,
+GatewayFromEnvironment,
+Connection,
+)
+from .ticks import (
+    Ticks,
+    )
+
+__all__ = [
+        'Candle', 'CandleMakerBase', 'CandleMakerTimed', 'CandleMakerCounted',
+        'CandleMakerDollarVolume', 'Gateway', 'TradersWorkstation', 'Btcjopaper',
+        'Btchfpaper', 'Nypaperib', 'Chpaperib', 'GatewayFromEnvironment', 'Connection',
+        'Gateway', 'TradersWorkstation', 'Btcjopaper', 'Btchfpaper', 'Nypaperib',
+        'Chpaperib', 'GatewayFromEnvironment', 'Connection',
+        ]
