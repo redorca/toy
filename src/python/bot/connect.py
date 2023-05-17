9cc557cc48de1b110531407c1e8cff763fe3d01a
Bill Rees <brees@dustyrobotics.com>
2023-05-17 12:20:31 -0700

    Abstract port and host used go connect
      So many hardcoded values for host, port, timeouts are no
      derived from the config_defaults dictionay

--- src/python/bot/connect.py
+++ src/python/bot/connect.py
@@ -3,25 +3,19 @@ import ib_insync as ibs
 
 from bot.crypto import CryptoConnection
 
-def connect(contract_type=None, client_id=11):
-    if contract_type == 'crypto':
-        return CryptoConnection()
-
-    ib = ibs.IB()
-    ib_gateway_port = 4002
-    ib_gateway_port_live = 4001
-    trader_workstation_port = 7496
-    # ib.connect('127.0.0.1', ib_gateway_port_live, clientId=client_id)  # IB Gateway
-    # ib.connect('127.0.0.1', ib_gateway_port, clientId=client_id)  # IB Gateway
-    # logger=logging.getLogger().setLevel(logging.DEBUG)
-    ib.connect('127.0.0.1', trader_workstation_port, clientId=client_id, timeout=20)  # Trader Workstation
-    return ib
-
 connection_defaults = {
     'ib_gateway_port_live': 4001,
     'ib_gateway_port': 4002,
     'trader_workstation_port': 7496,
+    'default': 4002,
+    'timeout': 10.0,
 }
+
+btchfpaper = connection_defaults.copy()
+btchfpaper.update(  {
+    'port': connection_defaults['default'],
+    'host': 'btchfpaper.rockyahoo.com',
+})
 
 btcjo = connection_defaults.copy()
 btcjo.update(    {
@@ -35,14 +29,31 @@ localhost.update({
     'port': connection_defaults["trader_workstation_port"]
 })
 
+def connect(config:dict=btchfpaper, contract_type=None, client_id=11):
+    if contract_type == 'crypto':
+        return CryptoConnection()
+
+    ib_gateway_port = config['port']
+    ib_gateway_host = config['host']
+    ib = ibs.IB()
+    # ib_gateway_port = 4002
+    # ib_gateway_port_live = 4001
+    # trader_workstation_port = 7496
+    # ib.connect('127.0.0.1', ib_gateway_port_live, clientId=client_id)  # IB Gateway
+    # ib.connect('127.0.0.1', ib_gateway_port, clientId=client_id)  # IB Gateway
+    # logger=logging.getLogger().setLevel(logging.DEBUG)
+    # ib.connect('127.0.0.1', trader_workstation_port, clientId=client_id, timeout=20)  # Trader Workstation
+    ib.connect(ib_gateway_host, ib_gateway_port, clientId=client_id, timeout=config['timeout'])
+    return ib
+
 async def connect_async(config:dict=btcjo, contract_type=None, client_id=11):
     if contract_type == 'crypto':
         return CryptoConnection()
 
     ib = ibs.IB()
-    ib_gateway_port = config['ib_gateway_port']
+    ib_gateway_port = config['port']
     ib_gateway_port_live = config['ib_gateway_port_live']
-    trader_workstation_port = config['trader_workstation_port']
+    trader_workstation_port = config['host']
     port = config["port"]
     # ib.connect('127.0.0.1', ib_gateway_port_live, clientId=client_id)  # IB Gateway
     # ib.connect('127.0.0.1', ib_gateway_port, clientId=client_id)  # IB Gateway
